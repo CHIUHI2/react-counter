f16c33b8282632a925712a152f2ab6b954f41c74
Hins Chiu <chiuhi2@oocl.com>
2020-12-07 18:22:37 +0800

    [counter][HinsChiu]feat:implement component Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            count: 0
+        };
+    }
+
+    onIncrease = () => {
+        this.setState(
+           prevState => ({ 
+               count : prevState.count + 1 
+            })
+        );
+    };
+    
+    onDecrease = () => {
+        this.setState(
+            prevState => ({ 
+                count : prevState.count - 1 
+            })
+        );
+    };
+    
+    render() {
+        return (
+            <div>
+                <input type="button" onClick={this.onDecrease} value="-" />
+                <span>{this.state.count}</span>
+                <input type="button" onClick={this.onIncrease} value="+" />
+            </div>
+        );
+    }
+}
+
+export default Counter;
